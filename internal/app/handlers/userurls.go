9308bb759b23d551b4006c8792407e3809583749
Ivan Mishin <imishin@samokat.ru>
2024-09-03 17:22:24 +0300

    iter15 fix

--- internal/app/handlers/userurls.go
+++ internal/app/handlers/userurls.go
@@ -28,24 +28,25 @@ func (h *ShortanerHandler) UserURLs(w http.ResponseWriter, r *http.Request) {
 	result := make([]UserURLsItem, 0)
 
 	if len(data) > 0 {
-		for k, v := range data {
-			result = append(
-				result,
-				UserURLsItem{Short: h.Options.BaseRedirectURL + "/" + k, Original: v},
-			)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-
-		out, err := json.Marshal(result)
-		if err != nil {
-			http.Error(w, "Error when create json", http.StatusInternalServerError)
-			return
-		}
-
-		w.Write(out)
-	} else {
 		w.WriteHeader(http.StatusNoContent)
+		return
 	}
+
+	for k, v := range data {
+		result = append(
+			result,
+			UserURLsItem{Short: h.Options.BaseRedirectURL + "/" + k, Original: v},
+		)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	out, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, "Error when create json", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(out)
 }
