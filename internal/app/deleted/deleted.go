8be960d28b13752a49785ff1aeb199644461ff23
Ivan Mishin <imishin@samokat.ru>
2024-10-21 11:13:09 +0300

    comment to errors

--- internal/app/deleted/deleted.go
+++ internal/app/deleted/deleted.go
@@ -1,14 +1,17 @@
 // Модуль deleted содержит ошибку об уже удаленных данных.
 package deleted
 
+// Основная структура ошибки.
 type DeletedError struct {
 	Err error
 }
 
+// Вывод ошибки
 func (e *DeletedError) Error() string {
 	return e.Err.Error()
 }
 
+// Инициализация ошибки
 func NewDeletedError(err error) error {
 	return &DeletedError{Err: err}
 }
--- internal/app/exsist/exist.go
+++ internal/app/exsist/exist.go
@@ -1,14 +1,17 @@
 // Модуль exist содержит ошибку об уже существующих данных.
 package exsist
 
+// Основная структура ошибки.
 type ExistError struct {
 	Err error
 }
 
+// Вывод ошибки
 func (e *ExistError) Error() string {
 	return e.Err.Error()
 }
 
+// Инициализация ошибки
 func NewExistError(err error) error {
 	return &ExistError{Err: err}
 }
